f6492ebd7d5d106859556fe1c9170e59bbbab744
Gwynard C. Andaya <87253742+PengGwyn@users.noreply.github.com>
2024-09-25 18:08:13 +0800

    added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TwoAutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timesOfClicksPerLocationInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clickPhaseTxt = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firstPosLbl = new System.Windows.Forms.Label();
+            this.secondPosLbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.clickCountLbl = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cyan;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(38, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(90, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Add Click Pos";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(42, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Clicking";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timesOfClicksPerLocationInput
+            // 
+            this.timesOfClicksPerLocationInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timesOfClicksPerLocationInput.Location = new System.Drawing.Point(153, 73);
+            this.timesOfClicksPerLocationInput.Name = "timesOfClicksPerLocationInput";
+            this.timesOfClicksPerLocationInput.Size = new System.Drawing.Size(70, 20);
+            this.timesOfClicksPerLocationInput.TabIndex = 3;
+            this.timesOfClicksPerLocationInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timesOfClicksPerLocationInput_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Repeatition Count";
+            // 
+            // clickPhaseTxt
+            // 
+            this.clickPhaseTxt.AutoSize = true;
+            this.clickPhaseTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickPhaseTxt.Location = new System.Drawing.Point(132, 118);
+            this.clickPhaseTxt.Name = "clickPhaseTxt";
+            this.clickPhaseTxt.Size = new System.Drawing.Size(103, 25);
+            this.clickPhaseTxt.TabIndex = 5;
+            this.clickPhaseTxt.Text = "1st Click";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(31, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "First Pos";
+            // 
+            // firstPosLbl
+            // 
+            this.firstPosLbl.AutoSize = true;
+            this.firstPosLbl.Location = new System.Drawing.Point(257, 67);
+            this.firstPosLbl.Name = "firstPosLbl";
+            this.firstPosLbl.Size = new System.Drawing.Size(16, 13);
+            this.firstPosLbl.TabIndex = 7;
+            this.firstPosLbl.Text = "---";
+            // 
+            // secondPosLbl
+            // 
+            this.secondPosLbl.AutoSize = true;
+            this.secondPosLbl.Location = new System.Drawing.Point(257, 103);
+            this.secondPosLbl.Name = "secondPosLbl";
+            this.secondPosLbl.Size = new System.Drawing.Size(16, 13);
+            this.secondPosLbl.TabIndex = 9;
+            this.secondPosLbl.Text = "---";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Second Pos";
+            // 
+            // clickCountLbl
+            // 
+            this.clickCountLbl.AutoSize = true;
+            this.clickCountLbl.Location = new System.Drawing.Point(276, 130);
+            this.clickCountLbl.Name = "clickCountLbl";
+            this.clickCountLbl.Size = new System.Drawing.Size(13, 13);
+            this.clickCountLbl.TabIndex = 10;
+            this.clickCountLbl.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(343, 175);
+            this.Controls.Add(this.clickCountLbl);
+            this.Controls.Add(this.secondPosLbl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.firstPosLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clickPhaseTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timesOfClicksPerLocationInput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Cyan;
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox timesOfClicksPerLocationInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label clickPhaseTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label firstPosLbl;
+        private System.Windows.Forms.Label secondPosLbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label clickCountLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoAutoClicker
+{
+    public partial class Form1 : Form
+    {
+        // Constants for mouse events
+        const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        // Import the necessary Windows API functions
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        private bool isReadingSecondClick = false;
+        private bool isClickingSecondPos = false;
+
+        private int firstClickPosX;
+        private int firstClickPosY;
+
+        private int secondClickPosX;
+        private int secondClickPosY;
+
+        private int repeatitionCount;
+
+        // This method simulates a mouse click at the given position
+        private void SimulateMouseClick(int x, int y)
+        {
+            // Set the cursor position (optional, can be skipped if you don't want to move the cursor)
+            // SetCursorPos(x, y);
+
+            // Perform the mouse down and up to simulate a left click
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)x, (uint)y, 0, 0);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            clickPhaseTxt.Text = "1st Pos";
+        }
+
+        private void timesOfClicksPerLocationInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the key is a control key (backspace, delete, etc.)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                // If the key is not a control key or a digit, block the input
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timesOfClicksPerLocationInput.Text == "")
+                return;
+
+            int clicks = Convert.ToInt32(timesOfClicksPerLocationInput.Text);
+            Console.WriteLine(clicks);
+            while (clicks >= 0)
+            {
+                clickCountLbl.Text = clicks.ToString();
+                if (!isClickingSecondPos)
+                {
+                    SimulateMouseClick(firstClickPosX, firstClickPosY);
+                    Cursor.Position = new Point(firstClickPosX, firstClickPosY);
+                    isClickingSecondPos = true;
+                }
+                else
+                {
+                    SimulateMouseClick(secondClickPosX, secondClickPosY);
+                    Cursor.Position = new Point(secondClickPosX, secondClickPosY);
+                    isClickingSecondPos = false;
+                }
+
+                // Sleep for 1 seconds (1000 milliseconds)
+
+                Thread.Sleep(1000);
+                clicks--;
+            }
+            clickPhaseTxt.Text = "1st Pos";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!isReadingSecondClick)
+            {
+                firstClickPosX = Cursor.Position.X;
+                firstClickPosY = Cursor.Position.Y;
+                isReadingSecondClick = true;
+                clickPhaseTxt.Text = "2nd Pos";
+                firstPosLbl.Text = "X: " + firstClickPosX.ToString() + " | Y: " + firstClickPosY.ToString();
+            }
+            else
+            {
+                secondClickPosX = Cursor.Position.X;
+                secondClickPosY = Cursor.Position.Y;
+                isReadingSecondClick = false;
+                clickPhaseTxt.Text = "Start Click";
+                secondPosLbl.Text = "X: " + secondClickPosX.ToString() + " | Y: " + secondClickPosY.ToString();
+            }
+        }
+    }
+}
